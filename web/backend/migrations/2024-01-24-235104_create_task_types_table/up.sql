00285b62ccc3385a5590c2a8df846cf7b8398394
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-02-09 12:02:41 +0100

    Added more tables

--- web/backend/migrations/2024-01-24-235104_create_task_types_table/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE task_types (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(512) NOT NULL
-);
--- web/backend/migrations/2024-02-06-123034_create_project_users_table/up.sql
+++ web/backend/migrations/2024-02-06-123034_create_project_users_table/up.sql
@@ -1,7 +1,7 @@
 -- SQL for creating the project_users table.
 -- The project_users table is used to store the users that are associated with a project.
 -- The user_id and project_id columns are used to store the user and project, which are used as primary keys.
--- The project_role column is used to store the role of the user in the project.
+-- The role column is used to store the role of the user in the project.
 -- The created_at column is used to store the creation time of the record.
 -- Since only a project administrator can add link a user to a project, the project_users table
 -- also contains a column to specify which administrator added the user to the project.
@@ -9,10 +9,10 @@
 CREATE TABLE project_users (
     user_id INT NOT NULL,
     project_id INT NOT NULL,
-    project_role INT NOT NULL,
+    role INT NOT NULL,
     added_by INT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, project_id, project_role),
+    PRIMARY KEY (user_id, project_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
     FOREIGN KEY (added_by) REFERENCES users (id) ON DELETE CASCADE
