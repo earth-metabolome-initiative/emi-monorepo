816c9e4d60422deb9c155c4198f167e488d8c907
PMA <pierre-marie.allard@unifr.ch>
2024-02-12 16:54:20 +0100

    sampled_individuals

--- web/backend/migrations/2024-02-12-153940_create_sampled_individuals_table/up.sql
+++ web/backend/migrations/2024-02-12-153940_create_sampled_individuals_table/up.sql
@@ -1,8 +1,8 @@
 -- Your SQL goes here
--- A migration to create the individuals table.
+-- A migration to create the sampled_individuals table.
 -- An individual is a physically identifiable member of a given species.
 
-CREATE TABLE individuals (
+CREATE TABLE sampled_individuals (
   id SERIAL PRIMARY KEY,
   item_id INTEGER NOT NULL REFERENCES items(id) ON DELETE CASCADE
 );
