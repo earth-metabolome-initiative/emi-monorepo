34b05a5e9ab8ede0174e9dc0223143c176275dff
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-02-15 16:28:39 +0100

    Resolved some problems in frontend

--- web/backend/src/main.rs
+++ web/backend/src/main.rs
@@ -24,36 +24,36 @@ async fn main() -> std::io::Result<()> {
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     // create db connection pool
-    // let manager = ConnectionManager::<PgConnection>::new(database_url);
-    // let pool: Pool = r2d2::Pool::builder()
-    //     // We set the maximum number of connections in the pool to 10
-    //     .max_size(10)
-    //     .build(manager)
-    //     .expect("Failed to create pool.");
-    // let domain: String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        // We set the maximum number of connections in the pool to 10
+        .max_size(10)
+        .build(manager)
+        .expect("Failed to create pool.");
+    let domain: String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
 
     // Start http server
-    // HttpServer::new(move || {
-    //     let cors = Cors::default()
-    //         .allowed_origin("http://localhost:3000")
-    //         .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
-    //         .allowed_headers(vec![
-    //             header::CONTENT_TYPE,
-    //             header::AUTHORIZATION,
-    //             header::ACCEPT,
-    //         ])
-    //         .supports_credentials();
-    //     App::new()
-    //         // pass in the database pool to all routes
-    //         .app_data(web::Data::new(pool.clone()))
-    //         // We register the API handlers
-    //         .configure(api::config)
-    //         // enable logger
-    //         .wrap(Logger::default())
-    //         // We add support for CORS requests
-    //         .wrap(cors)
-    //         // limit the maximum amount of data that server will accept
-    //         .app_data(web::JsonConfig::default().limit(4096))
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+        App::new()
+            // pass in the database pool to all routes
+            .app_data(web::Data::new(pool.clone()))
+            // We register the API handlers
+            .configure(api::config)
+            // enable logger
+            .wrap(Logger::default())
+            // We add support for CORS requests
+            .wrap(cors)
+            // limit the maximum amount of data that server will accept
+            .app_data(web::JsonConfig::default().limit(4096))
         // everything under '/api/' route
         // .service(
         //     web::scope("/api")
@@ -72,9 +72,8 @@ async fn main() -> std::io::Result<()> {
         //         //         .route(web::get().to(auth_handler::get_me)),
         //         // ),
         // )
-    // })
-    // .bind("127.0.0.1:8080")?
-    // .run()
-    // .await
-    Ok(())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
