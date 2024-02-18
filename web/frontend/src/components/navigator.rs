d92573fe3888cdc94ddbfead01379113485f38ca
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-02-18 17:56:01 +0100

    Continued work on frontend

--- web/frontend/src/components/navigator.rs
+++ web/frontend/src/components/navigator.rs
@@ -1,4 +1,4 @@
-//! The navigator Yew component is responsible for rendering the navigation bar at the top of the page.
+//! The navigator Yew 0.21 component is responsible for rendering the navigation bar at the top of the page.
 //! 
 //! Depending on whether we are on a large page, a middle page (such as table)
 //! or a small page (such as a mobile device), the navigation bar will render differently.
@@ -20,6 +20,10 @@
 //! user if they are offline by putting a badge with the text "Offline" on their avatar. Upon returning online,
 //! the badge will disappear.
 //! 
+//! The navigator component handles where and whether to show the search bar, the hamburger icon, the user name and avatar,
+//! the offline badge, the dropdown menu, and the side bar. When the screen is small, such as in a mobile device, the navigator
+//! will also handle the toggling of the side bar when the hamburger icon is clicked.
+//! 
 //! The styles of the navigator are defined using the Tailwind CSS framework.
 
 use yew::prelude::*;
@@ -29,32 +33,76 @@ use yewdux::prelude::*;
 use crate::components::search_bar::SearchBar;
 use crate::store::Store;
 
+use super::Sidebar;
 
-#[function_component(Navigator)]
-pub fn navigator() -> Html {
-    html! {
-        <nav class="bg-white shadow">
-            <div class="flex items center justify-between p-4">
-                <div class="flex items center">
-                    <button class="md:hidden" >
-                        <i class="fas fa-bars"></i>
-                    </button>
-                    <img class="h-8" src="/logo.svg" alt="logo" />
-                </div>
-                <SearchBar />
-                <div class="flex items center">
-                    <div class="flex items center">
-                        <img class="h-8 w-8 rounded-full" src="/avatar.jpg" alt="avatar" />
-                        <span class="ml-2 hidden md:block">{"John Doe"}</span>
-                        <div class="ml-2 hidden md:block">
-                            <i class="fas fa-caret-down"></i>
+
+pub struct Navigator {
+    sidebar: Sidebar,
+    search_bar: SearchBar,
+}
+
+impl Component for Navigator {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {
+            sidebar: Sidebar,
+            search_bar: SearchBar,
+        }
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn changed(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <nav class="bg-white shadow-lg">
+                <div class="container mx-auto px-4">
+                    <div class="flex justify-between items-center h-16">
+                        <div class="flex items center">
+                            <button class="text-gray-500 focus:outline-none lg:hidden">
+                                <i class="fas fa-bars"></i>
+                            </button>
+                            <a class="ml-4" href="/">
+                                <img class="h-8" src="/logo.svg" alt="logo" />
+                            </a>
+                        </div>
+                        <div class="hidden lg:flex items center">
+                            <self::SearchBar />
+                        </div>
+                        <div class="flex items center">
+                            <div class="relative">
+                                <button class="text-gray-500 focus:outline-none">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                <input
+                                    type="text"
+                                    class="w-64 h-10 px-3 pr-10 rounded-full border-2 border-gray-300 focus:outline-none focus:border-blue-500"
+                                    placeholder="Search..."
+                                />
+                            </div>
+                            <div class="ml-4 relative">
+                                <button class="text-gray-500 focus:outline-none">
+                                    <i class="fas fa-bell"></i>
+                                </button>
+                                <div class="absolute right-0 top-0 h-2 w-2 bg-red-500 rounded-full"></div>
+                            </div>
+                            <div class="ml-4 relative">
+                                <button class="text-gray-500 focus:outline-none">
+                                    <i class="fas fa-user"></i>
+                                </button>
+                                <div class="absolute right-0 top-0 h-2 w-2 bg-red-500 rounded-full"></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="ml-2 hidden md:block">
-                        <span class="bg-red-500 text-white rounded-full p-1">{"Offline"}</span>
-                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        }
     }
 }
--- web/frontend/src/components/search_bar.rs
+++ web/frontend/src/components/search_bar.rs
@@ -1,26 +1,38 @@
 //! A Yew-based search bar component to be placed in the navigator component.
-//! 
+//!
 //! The search bar is used to search for items in the application.
+//! When the screen is either large or medium, the search bar is shown in the center of the navigation bar.
+//! When the screen is small, such as in a mobile device, the search bar is shown on top of the side bar when the hamburger icon is clicked.
 //! 
+//! The search bar is a simple input field with smoothed corners, a search icon on the right (inside the input field),
+//! and a placeholder text "Search...". Furthermore, a search button is shown when the input field is focused or has text inside it.
+//! The search bar is also responsive, as it will shrink and grow depending on the size of the screen.
+//! Upon clicking the search button, the search bar will be cleared and the focus will be removed from it. The url is updated by
+//! using the Yew router, as we are working in a single page application.
+//!
 //! The styles of the search bar are defined using the Tailwind CSS framework.
-//! 
+//!
 
 use yew::prelude::*;
 use yewdux::prelude::*;
 
 use crate::store::Store;
 
+
 #[function_component(SearchBar)]
 pub fn search_bar() -> Html {
-    let (state, _) = use_store::<Store>();
-
-    let search_bar_classes = if state.sidebar_open {
-        "hidden md:block bg-gray-100 text-gray-700 rounded-lg p-2 pl-4"
-    } else {
-        "hidden md:block bg-gray-100 text-gray-700 rounded-lg p-2 pl-4"
-    };
-
     html! {
-        <input class={search_bar_classes} type="text" placeholder="Search..." />
+        <div class="relative">
+            <input
+                type="text"
+                class="w-64 h-10 px-3 pr-10 rounded-full border-2 border-gray-300 focus:outline-none focus:border-blue-500"
+                placeholder="Search..."
+            />
+            <button
+                class="absolute right-0 top-0 h-10 w-10 rounded-full bg-blue-500 text-white"
+            >
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
     }
 }
