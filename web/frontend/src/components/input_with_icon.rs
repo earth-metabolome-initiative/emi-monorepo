293c9e9bfe1b0dcc2a5628e7e1c414907292dc6c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-02-20 11:02:43 +0100

    Binded first few events to search bar

--- web/frontend/src/components/input_with_icon.rs
+++ web/frontend/src/components/input_with_icon.rs
@@ -9,14 +9,26 @@ use yew::prelude::*;
 pub struct InputWithIconProperties {
     pub icon: String,
     pub placeholder: Option<String>,
+    pub onsubmit: Callback<MouseEvent>,
+    #[prop_or_default]
+    pub oninput: Callback<InputEvent>,
+    #[prop_or_default]
+    pub button_disabled: bool,
+    #[prop_or_default]
+    pub input_disabled: bool,
 }
 
 #[function_component(InputWithIcon)]
 pub fn input_with_icon(props: &InputWithIconProperties) -> Html {
+    let onsubmit = props.onsubmit.reform(|mouse_event| mouse_event);
+    let oninput = props.oninput.reform(|input_event| input_event);
+
     html! {
         <div class="input-with-icon">
-            <input type="text" placeholder={props.placeholder.clone().unwrap_or_else(||"".to_string())} />
-            <i class={format!("fa fa-{}", props.icon)}></i>
+            <input type="text" placeholder={props.placeholder.clone().unwrap_or_else(||"".to_string())} disabled={props.input_disabled} oninput={oninput} />
+            <button type="button" disabled={props.button_disabled} onclick={onsubmit}>
+                <i class={format!("fa fa-{}", props.icon)}></i>
+            </button>
         </div>
     }
 }
--- web/frontend/src/components/search_bar.rs
+++ web/frontend/src/components/search_bar.rs
@@ -3,7 +3,7 @@
 //! The search bar is used to search for items in the application.
 //! When the screen is either large or medium, the search bar is shown in the center of the navigation bar.
 //! When the screen is small, such as in a mobile device, the search bar is shown on top of the side bar when the hamburger icon is clicked.
-//! 
+//!
 //! The search bar is a simple input field with smoothed corners, a search icon on the right (inside the input field),
 //! and a placeholder text "Search...". Furthermore, a search button is shown when the input field is focused or has text inside it.
 //! The search bar is also responsive, as it will shrink and grow depending on the size of the screen.
@@ -11,18 +11,38 @@
 //! using the Yew router, as we are working in a single page application.
 //!
 
+use crate::components::InputWithIcon;
 use yew::prelude::*;
 use yewdux::prelude::*;
-use crate::components::InputWithIcon;
+use web_sys::wasm_bindgen::UnwrapThrowExt;
+use web_sys::wasm_bindgen::JsCast;
 
 use crate::store::Store;
 
-
 #[function_component(SearchBar)]
 pub fn search_bar() -> Html {
+    // We prepare a boolean stored variable for the button_disabled property of the InputWithIcon component.
+    // The variable is updated by the oninput event of the input field.
+    let button_disabled = use_state(|| true);
+
+    // For the time being, the search bar does not do anything.
+    let onsubmit = { Callback::from(move |_| {}) };
+
+    // For the oninput event, when the user types in the search bar,
+    // for the time being we solely enable the search button.
+    let oninput = {
+        // We can clone the button_disabled state variable, as it is a reference counted pointer.
+        let button_disabled = button_disabled.clone();
+        Callback::from(move |input_event: InputEvent| {
+            // If the input field is empty, we disable the search button.
+            let input = input_event.target().unwrap_throw().dyn_into::<web_sys::HtmlInputElement>().unwrap_throw();
+            button_disabled.set(input.value().is_empty());
+        })
+    };
+
     html! {
         <div class="search">
-            <InputWithIcon icon="search" placeholder="Search..."/>
+            <InputWithIcon icon="search" placeholder="Search..." button_disabled={*button_disabled} onsubmit={onsubmit} oninput={oninput}/>
         </div>
     }
 }
