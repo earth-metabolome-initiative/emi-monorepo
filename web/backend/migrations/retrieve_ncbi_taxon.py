6674b2c4b84f5fffa2da3f4aa076a0640c14ccd9
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-04-04 15:08:18 +0200

    Added script to automatically retrieve taxons

---
+++ web/backend/migrations/retrieve_ncbi_taxon.py
@@ -0,0 +1,19 @@
+"""Retrieve the list of NCBI Taxons and writes it out to a file."""
+
+from downloaders import BaseDownloader
+import pandas as pd
+import shutil
+
+
+def retrieve_ncbi_taxon():
+    """Retrieve the list of NCBI Taxons and writes it out to a file."""
+    url = "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz"
+    BaseDownloader().download(url)
+
+    df = pd.read_csv("downloads/new_taxdump/rankedlineage.dmp", sep="\t", header=None)[
+        [0, 2]
+    ]
+    df.columns = ["taxon_id", "taxon_name"]
+    df.to_csv("../db_data/taxons.csv", sep="\t", index=False)
+    shutil.rmtree("downloads/new_taxdump")
+    shutil.rmtree("downloads/new_taxdump.tar.gz")
