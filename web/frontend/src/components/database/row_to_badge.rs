cbbd83e11541bd02fa3e7dcf0ec4796e49b5df57
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-04-18 17:39:38 +0200

    Updated RowToBadge trait

--- web/frontend/src/components/database/row_to_badge.rs
+++ web/frontend/src/components/database/row_to_badge.rs
@@ -2,9 +2,10 @@
 
 /// Trait for types that can be converted to a badge.
 pub trait RowToBadge {
-    /// The badge type that the implementing type can be converted to.
-    type Badge;
-
     /// Convert the implementing type to a badge.
-    fn to_badge(&self) -> Self::Badge;
+    /// 
+    /// # Arguments
+    /// * `target` - The optional target whose values are to be highlighted
+    ///              using the sublime_fuzzy best_match and format_simple methods.
+    fn to_badge(&self, target: Option<&str>) -> yew::Html;
 }
