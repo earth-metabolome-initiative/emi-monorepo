6acfaba99528934f3432211349919ddf931451d2
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-04-28 14:49:21 +0200

    removed commented code

--- web/backend/migrations/00000000000030_create_bio_ott_taxon_items_index/up.sql
+++ web/backend/migrations/00000000000030_create_bio_ott_taxon_items_index/up.sql
@@ -3,4 +3,3 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 CREATE INDEX bio_ott_taxon_items_name_trgm_idx ON bio_ott_taxon_items USING gin (name gin_trgm_ops);
--- CREATE INDEX bio_ott_taxon_items_name_trgm_idx ON bio_ott_taxon_items USING gist (name gist_trgm_ops);
