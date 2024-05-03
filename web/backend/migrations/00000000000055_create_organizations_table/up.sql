37b80114cc51a3a2664b703146ef885769381850
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-05-03 15:24:43 +0200

    Fixed error in organizations table

--- web/backend/migrations/00000000000055_create_organizations_table/up.sql
+++ web/backend/migrations/00000000000055_create_organizations_table/up.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS organizations (
     description TEXT NOT NULL,
     parent_organization_id INTEGER DEFAULT NULL REFERENCES organizations(id) ON
     DELETE
-        CASCADE,
-        name TEXT NOT NULL
+        CASCADE
 );
