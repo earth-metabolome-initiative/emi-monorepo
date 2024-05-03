dcb22de26af5a252388ee6d494602e5bd55c33fe
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-05-03 12:00:37 +0200

    Added more self-consistency checks for the migrations

--- web/backend/migrations/generate_models.py
+++ web/backend/migrations/generate_models.py
@@ -4788,6 +4788,21 @@ def check_for_common_typos_in_migrations():
                 f"Migration `{directory}` contains a typo: `DROP TABLE IF NOT EXISTS` instead of `DROP TABLE IF EXISTS`."
             )
 
+        # If there is a creation of a temporary table in a up or down migration, in the same document there
+        # must be a deletion of the temporary table.
+        for content, content_name in (
+            (up_content, "up"),
+            (down_content, "down"),
+        ):
+            if "CREATE TEMPORARY TABLE" in content:
+                # We retrieve the name of the temporary table.
+                table_name = content.split("CREATE TEMPORARY TABLE")[1].split("(")[0].strip()
+                # We check that the deletion of the temporary table is present in the up migration.
+                if f"DROP TABLE {table_name}" not in content:
+                    raise Exception(
+                        f"Migration `{directory}` contains a `CREATE TEMPORARY TABLE` constraint in the {content_name}.sql file, but does not contain a `DROP TABLE {table_name}` constraint in the {content_name}.sql file."
+                    )
+
 
 def ensures_gluesql_compliance():
     for directory in os.listdir("migrations"):
@@ -4880,6 +4895,13 @@ def enforce_migration_naming_convention():
             up_content = up_file.read()
 
         expected_name = directory
+
+        if "CREATE TEMP TABLE" in up_content:
+            raise Exception(
+                f"Migration {directory} contains a `CREATE TEMP TABLE` constraint. Please standardize the naming convention "
+                "and replace it with `CREATE TEMPORARY TABLE`."
+            )
+
 
         if "CREATE TABLE IF NOT EXISTS" in up_content:
             # This document contains a table creation, and as such
