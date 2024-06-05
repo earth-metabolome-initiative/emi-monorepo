9c386f254e715dbff894f139710ba80d84534ccb
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-06-05 23:41:19 +0200

    Improved query matches highlighting

--- web/frontend/src/traits/format_match.rs
+++ web/frontend/src/traits/format_match.rs
@@ -2,6 +2,18 @@
 //! for all types that implement ToString.
 use sublime_fuzzy::{best_match, format_simple};
 
+fn custom_best_matches<S: AsRef<str>, T: AsRef<str>>(
+    query: S,
+    target: T,
+) -> Vec<sublime_fuzzy::Match> {
+    query
+        .as_ref()
+        .split_whitespace()
+        .map(|query| best_match(query, target.as_ref()))
+        .filter_map(|x| x)
+        .collect::<Vec<sublime_fuzzy::Match>>()
+}
+
 /// Trait formatting string for a given query using sublime_fuzzy.
 pub trait FormatMatch {
     /// Format the string for a given query using sublime_fuzzy.
@@ -23,25 +35,74 @@ pub trait FormatMatch {
     fn similarity_score<S: AsRef<str>>(&self, query: S) -> isize;
 }
 
-impl<T: ToString> FormatMatch for T {
+impl<T: AsRef<str>> FormatMatch for T {
     fn format_match<S: AsRef<str>>(&self, query: S) -> yew::Html {
-        let current = self.to_string();
-        yew::Html::from_html_unchecked(yew::AttrValue::from(
-            best_match(query.as_ref(), &current).map_or_else(
-                || current.clone(),
-                |match_value| format_simple(&match_value, &current, "<strong>", "</strong>"),
-            ),
-        ))
+        let mut matches = custom_best_matches(query, self);
+        if matches.is_empty() {
+            yew::Html::from(self.as_ref().to_string())
+        } else {
+            // In order to properly format the string, we need to sort the matches
+            // by their start index. If it happens that after sorting a match starts
+            // before the previous match ends, we keep the largest match.
+            matches.sort_by(|a, b| a.matched_indices().min().cmp(&b.matched_indices().min()));
+
+            let start = matches
+            .first()
+            .unwrap()
+            .matched_indices()
+            .min()
+            .unwrap()
+            .clone();
+            let mut prev_end = matches
+                .first()
+                .unwrap()
+                .matched_indices()
+                .max()
+                .unwrap()
+                .clone() + 1;
+
+            let mut formatted = self.as_ref()[..start]
+                .to_string();
+
+            formatted.push_str("<strong>");
+            formatted.push_str(&self.as_ref()[start..prev_end]);
+            formatted.push_str("</strong>");
+
+            for m in matches.into_iter().skip(1) {
+                let start = m.matched_indices().min().unwrap().clone();
+                let end = m.matched_indices().max().unwrap().clone() + 1;
+
+                if start > prev_end {
+                    formatted.push_str(&self.as_ref()[prev_end..start]);
+                    formatted.push_str("<strong>");
+                    formatted.push_str(&self.as_ref()[start..end]);
+                    formatted.push_str("</strong>");
+                } else {
+                    continue;
+                }
+
+                prev_end = end;
+            }
+
+            formatted.push_str(&self.as_ref()[prev_end..]);
+
+            yew::Html::from_html_unchecked(yew::AttrValue::from(
+                formatted
+            ))
+        }
     }
 
     fn maybe_format_match<S: AsRef<str>>(&self, query: Option<S>) -> yew::Html {
         query
             .map(|query| self.format_match(query))
-            .unwrap_or_else(|| yew::Html::from(self.to_string()))
+            .unwrap_or_else(|| yew::Html::from(self.as_ref().to_string()))
     }
 
     fn similarity_score<S: AsRef<str>>(&self, query: S) -> isize {
-        let current = self.to_string();
-        best_match(query.as_ref(), &current).map_or(0, |match_value| match_value.score())
+        let matches = custom_best_matches(query, self);
+        if matches.is_empty() {
+            return 0;
+        }
+        matches.iter().map(|m| m.score()).sum::<isize>() / matches.len() as isize
     }
 }
