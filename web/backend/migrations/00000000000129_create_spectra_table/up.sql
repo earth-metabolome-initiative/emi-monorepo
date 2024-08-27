4a2bb9dcb9e763746e4a347265969dc5298106fd
oolonek <pierre-marie.allard@unifr.ch>
2024-08-27 11:09:28 +0200

    updated spectra table

--- web/backend/migrations/00000000000129_create_spectra_table/up.sql
+++ web/backend/migrations/00000000000129_create_spectra_table/up.sql
@@ -1,6 +1,20 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS spectra (
-    id INTEGER PRIMARY KEY,
+    feature_id INTEGER NOT NULL,
     spectra_collection_id INTEGER NOT NULL,
-    FOREIGN KEY (spectra_collection_id) REFERENCES spectra_collections(id) ON DELETE CASCADE
+    precursor_mz REAL NOT NULL,
+    -- We keep the retention time as optional for now to handl, for ex., in silico spectral datababases
+    retention_time REAL,
+    charge INTEGER NOT NULL,
+    -- The following field could be improved to polarity and an enum (positive, negative). We have to chek for GlueSQL compliance here.
+    positive BOOLEAN NOT NULL,
+    ms_level INTEGER NOT NULL,
+    mz REAL[] NOT NULL,
+    intensity REAL[] NOT NULL,
+    FOREIGN KEY (spectra_collection_id) REFERENCES spectra_collections(id) ON DELETE CASCADE,
+    PRIMARY KEY (spectra_collection_id, feature_id, ms_level),
+    CHECK (precursor_mz >= 0),
+    CHECK (retention_time >= 0),
+    CHECK (ms_level >= 1),
+    CHECK array_length(mz, 1) = array_length(intensity, 1)
 );
