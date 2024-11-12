458ab97b814649ef9d6f41c8e79e3fce4249891f
Marco Visani <marco.visani@unifr.ch>
2024-11-12 10:26:57 +0100

    added test for singularisation of words

---
+++ web/web_common/webcodegen/tests/test_singularize_words.rs
@@ -0,0 +1,58 @@
+use pluralizer;
+use regex::Regex;
+#[test]
+fn singluarize() -> () {
+    pluralizer::add_singular_rule(
+        Regex::new("(?i)(spectr|append)a$").unwrap(),
+        "$1um".to_string(),
+    );
+    pluralizer::add_singular_rule(
+        Regex::new("(?i)(matr|append)ices$").unwrap(),
+        "$1ix".to_string(),
+    );
+    pluralizer::add_singular_rule(
+        Regex::new("(?i)(tax|append)a$").unwrap(),
+        "$1on".to_string(),
+    );
+
+    let words = vec![
+        "Taxa",
+        "elephants",
+        "taxal",
+        "matrices",
+        "tables",
+        "houses",
+        "columns",
+        "spectra",
+        "is_nullable",
+        "is_self_referencing",
+        "is_identity",
+        "identity_generation",
+        "identity_start",
+        "identity_increment",
+    ];
+
+    let expected = vec![
+        "Taxon",
+        "elephant",
+        "taxal",
+        "matrix",
+        "table",
+        "house",
+        "column",
+        "spectrum",
+        "is_nullable",
+        "is_self_referencing",
+        "is_identity",
+        "identity_generation",
+        "identity_start",
+        "identity_increment",
+    ];
+
+    let singularized = words
+        .iter()
+        .map(|word| pluralizer::pluralize(word, 1, false))
+        .collect::<Vec<String>>();
+
+    assert_eq!(singularized, expected);
+}
