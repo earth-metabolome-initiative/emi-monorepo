c71b15b35fd4495f2cce885038ea1d4507c03b65
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-12-13 14:38:39 +0100

    Started to setup cross-compilation of validation functions

---
+++ web/web_common/validations/src/lib.rs
@@ -0,0 +1,35 @@
+use pgrx::prelude::*;
+
+::pgrx::pg_module_magic!();
+
+#[pg_extern]
+fn hello_validations() -> &'static str {
+    "Hello, validations"
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgrx::prelude::*;
+
+    #[pg_test]
+    fn test_hello_validations() {
+        assert_eq!("Hello, validations", crate::hello_validations());
+    }
+
+}
+
+/// This module is required by `cargo pgrx test` invocations.
+/// It must be visible at the root of your extension crate.
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    #[must_use]
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
