e4747d5ce4709c136ed375e7f27c5e42064636dd
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-12-19 16:53:35 +0100

    Working on new db

---
+++ shell.nix
@@ -0,0 +1,57 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # Basic build inputs
+    zlib
+    readline
+    openssl
+    libxml2
+    icu
+    libuuid
+
+    # LLVM for JIT support
+    llvmPackages.llvm
+    llvmPackages.llvm.dev
+
+    # Compression libraries
+    lz4
+    zstd
+
+    # System libraries
+    systemd
+    linux-pam
+
+    # Python support
+    python3
+
+    # Kerberos support
+    libkrb5
+
+    # Native build inputs
+    makeWrapper
+    pkg-config
+    removeReferencesTo
+    nukeReferences
+
+    # Documentation and parsing tools
+    bison
+    flex
+    perl
+    docbook_xml_dtd_45
+    docbook-xsl-nons
+    libxslt
+    rustup
+    cargo
+    cargo-pgrx
+
+    glibc.dev
+    clang
+  ];
+
+  # Set up environment variables if needed
+  shellHook = ''
+    export PGDATA="$PWD/data"
+    export PGHOST="$PWD/run"
+  '';
+}
