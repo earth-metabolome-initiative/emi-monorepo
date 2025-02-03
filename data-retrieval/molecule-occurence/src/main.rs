9c40f7558c74fa5c69986bc48fe540fa9aa9b717
Marco Visani <marco.visani@unifr.ch>
2025-02-03 18:32:36 +0100

    switched to regex search

--- data-retrieval/molecule-occurence/src/main.rs
+++ data-retrieval/molecule-occurence/src/main.rs
@@ -3,10 +3,9 @@
 //!
 //! Pubmed is split into difference files so we are going to download one by one, parse it and count the occurence of each molecule.
 
-use downloader::{CompressionExtension, Downloader, Task};
-use indicatif::{ProgressBar, ProgressIterator};
+use downloader::{Downloader, Task};
+use indicatif::ProgressBar;
 use rayon::prelude::*;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::io::{BufRead, BufReader};
@@ -28,6 +27,23 @@ fn read_molecule_names() -> Vec<String> {
     molecule_names
 }
 
+async fn single_task(file_number: usize, molecule_names: &Vec<String>) {
+    let url = get_pubmed_url_from_counter(file_number);
+    let task: Downloader = Downloader::default()
+        .extract()
+        .show_loading_bar()
+        .task(
+            Task::try_from(url)
+                .unwrap()
+                .target_path(&format!("pubmed25n{:04}.xml.gz", file_number)),
+        )
+        .unwrap();
+    task.execute().await.unwrap();
+
+    let file_name = format!("pubmed25n{:04}.xml", file_number);
+    let text = std::fs::read_to_string(&file_name).unwrap();
+}
+
 #[tokio::main]
 async fn main() {
     let task: Downloader = Downloader::default()
@@ -41,8 +57,7 @@ async fn main() {
         .unwrap();
     task.execute().await.unwrap();
 
-    let file = File::open("pubmed25n0001.xml").unwrap();
-    let reader = BufReader::new(file);
+    let text = std::fs::read_to_string("pubmed25n0001.xml").unwrap();
 
     let molecule_names = read_molecule_names();
     let pb = ProgressBar::new(molecule_names.len() as u64);
@@ -53,24 +68,22 @@ async fn main() {
             .progress_chars("#>-"),
     );
 
-    let lines = reader
-        .lines()
-        .map(|line| line.unwrap())
-        .collect::<Vec<String>>();
-
     // we create a vector of 0 of the same length as the molecule_names
     let mut molecule_counts: Vec<usize> = vec![0; molecule_names.len()];
+
+    // we create the regex for each molecule
+    let res = molecule_names
+        .iter()
+        .map(|molecule| regex::Regex::new(regex::escape(&molecule.as_str()).as_str()).unwrap())
+        .collect::<Vec<regex::Regex>>();
 
     molecule_counts
         .par_iter_mut()
         .enumerate()
         .for_each(|(i, count)| {
-            let molecule = &molecule_names[i];
-            for line in lines.iter() {
-                if line.contains(molecule) {
-                    *count += 1;
-                }
-            }
+            let re = &res[i];
+            let res = re.find_iter(&text).map(|m| m.as_str()).count();
+            *count = res;
             pb.inc(1);
         });
 
@@ -78,5 +91,31 @@ async fn main() {
     let mut file = File::create("molecule_occurence.tsv").unwrap();
     for (molecule, count) in molecule_names.iter().zip(molecule_counts.iter()) {
         writeln!(file, "{}\t{}", molecule, count).unwrap();
+    }
+}
+
+fn get_pubmed_url_from_counter(counter: usize) -> String {
+    format!(
+        "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed25n{:04}.xml.gz",
+        counter
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_pubmed_url_from_counter() {
+        let url = get_pubmed_url_from_counter(27);
+        assert_eq!(
+            url,
+            "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed25n0027.xml.gz"
+        );
+
+        assert_eq!(
+            get_pubmed_url_from_counter(352),
+            "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed25n0352.xml.gz"
+        )
     }
 }
