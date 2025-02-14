0b6e3c5c8bfeb6a6e5043a2a6c4fd99804bad2eb
Marco Visani <marco.visani@unifr.ch>
2025-02-14 11:43:59 +0100

    launched fetching of pubchem

--- data-retrieval/molecule-occurence/src/main.rs
+++ data-retrieval/molecule-occurence/src/main.rs
@@ -10,9 +10,8 @@ use std::fs::File;
 use std::io::Write;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
-
-const PUBMED_URL: &'static str =
-    "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed25n0001.xml.gz";
+use tokio::task;
+use tokio::time::error::Error;
 
 fn read_molecule_names() -> Vec<String> {
     let path = Path::new("molecule_names.txt");
@@ -27,7 +26,14 @@ fn read_molecule_names() -> Vec<String> {
     molecule_names
 }
 
-async fn single_task(file_number: usize, molecule_names: &Vec<String>) {
+async fn single_task(file_number: usize, molecule_names: &Vec<String>) -> Result<(), Error> {
+    // check if output file already exists
+    let out_file_name = format!("results/molecule_occurence_{:04}.tsv", file_number);
+    if Path::new(&out_file_name).exists() {
+        println!("File {} already exists. Skipping...", out_file_name);
+        return Ok(());
+    }
+
     let url = get_pubmed_url_from_counter(file_number);
     let task: Downloader = Downloader::default()
         .extract()
@@ -42,36 +48,8 @@ async fn single_task(file_number: usize, molecule_names: &Vec<String>) {
 
     let file_name = format!("pubmed25n{:04}.xml", file_number);
     let text = std::fs::read_to_string(&file_name).unwrap();
-}
 
-#[tokio::main]
-async fn main() {
-    let task: Downloader = Downloader::default()
-        .extract()
-        .show_loading_bar()
-        .task(
-            Task::try_from(PUBMED_URL)
-                .unwrap()
-                .target_path(&"pubmed25n0001.xml.gz"),
-        )
-        .unwrap();
-    task.execute().await.unwrap();
-
-    let text = std::fs::read_to_string("pubmed25n0001.xml").unwrap();
-
-    let molecule_names = read_molecule_names();
-    let pb = ProgressBar::new(molecule_names.len() as u64);
-    pb.set_style(
-        indicatif::ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({eta})")
-            .unwrap()
-            .progress_chars("#>-"),
-    );
-
-    // we create a vector of 0 of the same length as the molecule_names
     let mut molecule_counts: Vec<usize> = vec![0; molecule_names.len()];
-
-    // we create the regex for each molecule
     let res = molecule_names
         .iter()
         .map(|molecule| regex::Regex::new(regex::escape(&molecule.as_str()).as_str()).unwrap())
@@ -84,11 +62,58 @@ async fn main() {
             let re = &res[i];
             let res = re.find_iter(&text).map(|m| m.as_str()).count();
             *count = res;
-            pb.inc(1);
         });
 
-    // we write the results to a tsv
-    let mut file = File::create("molecule_occurence.tsv").unwrap();
+    // we create a directory to store the results if it does not exist
+    std::fs::create_dir_all("results").unwrap();
+
+    let mut file = File::create(out_file_name).unwrap();
+    for (molecule, count) in molecule_names.iter().zip(molecule_counts.iter()) {
+        writeln!(file, "{}\t{}", molecule, count).unwrap();
+    }
+
+    // remove the downloaded file
+    std::fs::remove_file(&file_name).unwrap();
+    std::fs::remove_file(format!("pubmed25n{:04}.xml.gz", file_number)).unwrap();
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let n_cpus = num_cpus::get() / 2;
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(n_cpus)
+        .build_global()
+        .unwrap();
+    let molecule_names = read_molecule_names();
+    let n_files = 1274;
+
+    let pb = ProgressBar::new(n_files as u64);
+
+    for file_number in 1..=n_files {
+        let molecule_names = molecule_names.clone(); // Clone per task
+        let pb = pb.clone(); // Clone progress bar
+        let _ = single_task(file_number, &molecule_names).await;
+        pb.inc(1);
+    }
+
+    // now we collect the results
+    let mut molecule_counts: Vec<usize> = vec![0; molecule_names.len()];
+    for file_number in 1..=n_files {
+        let out_file_name = format!("results/molecule_occurence_{:04}.tsv", file_number);
+        let file = File::open(out_file_name).unwrap();
+        let reader = BufReader::new(file);
+
+        for (i, line) in reader.lines().enumerate() {
+            let line = line.unwrap();
+            let mut parts = line.split("\t");
+            let molecule = parts.next().unwrap();
+            let count = parts.next().unwrap().parse::<usize>().unwrap();
+            molecule_counts[i] += count;
+        }
+    }
+
+    let mut file = File::create("molecule_occurence_final.tsv").unwrap();
     for (molecule, count) in molecule_names.iter().zip(molecule_counts.iter()) {
         writeln!(file, "{}\t{}", molecule, count).unwrap();
     }
@@ -117,5 +142,11 @@ mod tests {
             get_pubmed_url_from_counter(352),
             "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed25n0352.xml.gz"
         )
+    }
+
+    #[tokio::test]
+    async fn single_task_test() {
+        let molecule_names = read_molecule_names();
+        let _ = single_task(1, &molecule_names).await.is_ok();
     }
 }
