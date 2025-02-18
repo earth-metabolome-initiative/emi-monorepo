cb0194bbf97a410dbb68bf38380194e32684ec1a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-02-18 13:00:44 +0100

    Integrated time requirements analysis

--- web/core_structures/build.rs
+++ web/core_structures/build.rs
@@ -8,6 +8,7 @@ use taxonomy_fetcher::{
     impls::ncbi::{NCBIRank, NCBITaxonomy, NCBITaxonomyBuilder},
     Rank, Taxonomy, TaxonomyBuilder,
 };
+use time_requirements::prelude::*;
 use webcodegen::{
     AuthorizationFunctionBuilder, Codegen, CompatibleForeignTypeConstraint, CustomColumnConstraint,
     CustomTableConstraint, LowercaseColumnConstraint, LowercaseTableConstraint, Table,
@@ -41,6 +42,8 @@ fn establish_connection_to_postgres() -> PgConnection {
 #[tokio::main]
 pub async fn main() {
     // We ensure that the migrations directory has all expected properties.
+    let mut time_tracker = TimeTracker::new("Building Core Structures");
+    let task = Task::new("Checking Migrations Directory");
     let mut extension_migrations = MigrationDirectory::try_from("extensions_migrations").unwrap();
     if !extension_migrations.is_dense() {
         extension_migrations = extension_migrations.redensify().unwrap();
@@ -49,19 +52,29 @@ pub async fn main() {
     if !migrations.is_dense() {
         migrations = migrations.redensify().unwrap();
     }
+    time_tracker.add_completed_task(task);
 
     // First, we create the CSV for the font-awesome icons
+    let task = Task::new("Creating Font Awesome Icons CSV");
     font_awesome::Icon::to_csv("csvs/icons.csv").unwrap();
+    time_tracker.add_completed_task(task);
 
     // Next, we create the CSV for the taxonomical ranks
+    let task = Task::new("Creating Taxonomical Ranks CSV");
     NCBIRank::to_csv("csvs/ranks.csv").unwrap();
+    time_tracker.add_completed_task(task);
 
     // We retrieve and build the latest version of the NCBI taxonomy
+    let task = Task::new("Fetching NCBI Taxonomy");
     let taxonomy: NCBITaxonomy = NCBITaxonomyBuilder::latest().build().await.unwrap();
+    time_tracker.add_completed_task(task);
+    let task = Task::new("Creating Taxonomy CSV");
     taxonomy.to_csv("csvs/taxa.csv").unwrap();
+    time_tracker.add_completed_task(task);
 
     // Next, we build the SQL associated with the CSVs present in the 'csvs'
     // directory
+    let task = Task::new("Building SQL from CSVs");
     let schema: CSVSchema = CSVSchemaBuilder::default()
         // To show a loading bar while processing the CSVs
         .verbose()
@@ -76,10 +89,14 @@ pub async fn main() {
 
     // We create the SQL to create the tables and populate them
     let sql: String = schema.to_postgres().unwrap();
+    time_tracker.add_completed_task(task);
 
     // And obtain a connection to the database
+    let task = Task::new("Establishing Connection to Postgres");
     let mut conn = establish_connection_to_postgres();
+    time_tracker.add_completed_task(task);
 
+    let task = Task::new("Executing CSQLV SQL");
     // We execute the SQL
     if let Err(error) = conn.batch_execute(&sql) {
         // We write out to file the sql we have just executed that has failed.
@@ -88,29 +105,36 @@ pub async fn main() {
         eprintln!("Failed to execute SQL: {:?}", error);
         std::process::exit(1);
     }
+    time_tracker.add_completed_task(task);
 
     // We execute the migrations
+    let task = Task::new("Executing Migrations");
     for migration in extension_migrations.ups().unwrap() {
         conn.batch_execute(&migration).unwrap();
     }
     for migration in migrations.ups().unwrap() {
         conn.batch_execute(&migration).unwrap();
     }
+    time_tracker.add_completed_task(task);
 
     // Now that the preliminary database setup is done, we can execute the Meta-SQL
     // which takes care of the roles tables and canx functions, which determine
     // whether a user can insert or update entries in a given table.
+    let task = Task::new("Executing Meta-SQL");
     Table::create_roles_tables(&mut conn, DATABASE_NAME, None).unwrap();
     AuthorizationFunctionBuilder::default()
         .add_childless_table(Table::load(&mut conn, "users", None, DATABASE_NAME).unwrap())
         .create_authorization_functions_and_triggers(&mut conn, DATABASE_NAME, None)
         .unwrap();
     Table::create_update_triggers(&mut conn, DATABASE_NAME, None).unwrap();
+    time_tracker.add_completed_task(task);
 
     // We check that the database follows the expected constraints.
+    let task = Task::new("Checking Constraints");
     CompatibleForeignTypeConstraint::default().check_all(DATABASE_NAME, None, &mut conn).unwrap();
     LowercaseColumnConstraint::default().check_all(DATABASE_NAME, None, &mut conn).unwrap();
     LowercaseTableConstraint::default().check_all(DATABASE_NAME, None, &mut conn).unwrap();
+    time_tracker.add_completed_task(task);
 
     // We write to the target directory the generated structs
 
@@ -121,8 +145,23 @@ pub async fn main() {
     let path = Path::new(&out_dir).join("core_structs.rs");
 
     // Generate the code associated with the database
+    let task = Task::new("Generating Code");
     Codegen::default()
         .set_output_path(path.as_ref())
         .generate(&mut conn, DATABASE_NAME, None)
+        .unwrap();
+    time_tracker.add_completed_task(task);
+
+    // We save the time tracker
+    time_tracker.save(Path::new("time_tracker")).unwrap();
+
+    // We print the report
+    let mut report = Report::new(time_tracker);
+    report.add_directory(Path::new("time_tracker")).unwrap();
+    report
+        .write(
+            &Path::new("time_requirements_report.md"),
+            &Path::new("time_requirements_report.png"),
+        )
         .unwrap();
 }
