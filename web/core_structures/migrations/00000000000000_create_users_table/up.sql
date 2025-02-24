663ba8d798c4538cbed4964851d1ecefbcfa392c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-02-24 16:01:05 +0100

    Added user organizations to allow for multiple user orgs

--- web/core_structures/migrations/00000000000000_create_users_table/up.sql
+++ web/core_structures/migrations/00000000000000_create_users_table/up.sql
@@ -1,14 +1,22 @@
--- Your SQL goes here
+-- We need to create a table to store users
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     picture BYTEA NOT NULL,
-    organization_id SMALLINT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (organization_id) REFERENCES organizations(id)
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- We need to create an index on the name column to speed up queries
 CREATE INDEX IF NOT EXISTS users_name_trgm_idx ON users USING gin (
   name gin_trgm_ops
+);
+
+-- Since users may have multiple organizations, we need a join table to represent this relationship
+CREATE TABLE IF NOT EXISTS user_organizations (
+    user_id INTEGER NOT NULL,
+    organization_id SMALLINT NOT NULL,
+    PRIMARY KEY (user_id, organization_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE
 );
