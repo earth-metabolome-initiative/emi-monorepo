5d4bc242cc9df0ec7b789dcf035051587cdf5691
oolonek <pierre-marie.allard@unifr.ch>
2025-02-25 16:01:11 +0100

    added another unique foreign table

--- web/web_common/webcodegen/test_migrations/2024-11-08-212004_samples_table/up.sql
+++ web/web_common/webcodegen/test_migrations/2024-11-08-212004_samples_table/up.sql
@@ -5,6 +5,8 @@ CREATE TABLE samples (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by INTEGER NOT NULL,
     updated_by INTEGER NOT NULL,
+    spectrum_id INTEGER NOT NULL,
     FOREIGN KEY (created_by) REFERENCES users(id),
-    FOREIGN KEY (updated_by) REFERENCES users(id)
+    FOREIGN KEY (updated_by) REFERENCES users(id),
+    FOREIGN KEY (spectrum_id) REFERENCES spectra(id)
 );
