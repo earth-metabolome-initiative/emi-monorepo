0ca2be1f579fafd140aa90792fe954e5b1f62f5d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-03-06 20:05:34 +0100

    Trying to fix collisions

--- web/web_common/webcodegen/src/table_metadata/check_constraint.rs
+++ web/web_common/webcodegen/src/table_metadata/check_constraint.rs
@@ -6,8 +6,6 @@ use diesel::{
 use super::{Column, PgConstraint, PgExtension, PgProc};
 use crate::errors::WebCodeGenError;
 
-use super::Column;
-
 #[derive(Queryable, QueryableByName, Debug, Selectable)]
 #[diesel(table_name = crate::schema::check_constraints)]
 /// A struct representing a check constraint
