4ea0dd726fc4f80728647be9ce5f93c3f0434076
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-03-25 15:28:51 +0100

    Now cosine similarity returns also the number of matching peaks

--- utils/functional-properties/src/similarity.rs
+++ utils/functional-properties/src/similarity.rs
@@ -1,11 +1,9 @@
 //! A submodule defining similarities.
 
-use algebra::prelude::Number;
-
 /// Trait for calculating the similarity between two items.
 pub trait ScalarSimilarity<Left, Right> {
     /// The type of the similarity.
-    type Similarity: Number;
+    type Similarity;
 
     /// Calculate the similarity between two items.
     fn similarity(&self, left: &Left, right: &Right) -> Self::Similarity;
