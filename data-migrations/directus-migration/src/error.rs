7032067258905ebef12cc9d25f80e1d6cd5b1c58
edouardbruelhart <edouard.bruelhart@unifr.ch>
2025-03-26 16:10:50 +0100

    added user table

--- data-migrations/directus-migration/src/error.rs
+++ data-migrations/directus-migration/src/error.rs
@@ -1,14 +1,23 @@
 //! Enumeration of errors that may occur during directus migration
 
+use web_common_traits::database::InsertError;
+use core_structures::codegen::structs_codegen::tables::insertables::InsertableUserAttributes;
+
 #[derive(Debug)]
 /// Enumeration of errors that may occur during directus migration
 pub enum Error{
     /// Error when user doesn't have an email
     MissingEmail(uuid::Uuid),
+    /// Missing first name
+    MissingFirstName(uuid::Uuid),
+    /// Missing last name
+    MissingLastName(uuid::Uuid),
     /// Failed to establish database connection
     ConnectionFailed(diesel::ConnectionError),
     /// Failed to execute a query
     QueryFailed(diesel::result::Error),
+    /// Failer to insert user
+    UserInsertError(InsertError<InsertableUserAttributes>),
 }
 
 impl From<diesel::ConnectionError> for Error{
@@ -20,5 +29,11 @@ impl From<diesel::ConnectionError> for Error{
 impl From<diesel::result::Error> for Error{
     fn from(value: diesel::result::Error) -> Self {
         Error::QueryFailed(value)
+    }
+}
+
+impl From<InsertError<InsertableUserAttributes>> for Error{
+    fn from(value: InsertError<InsertableUserAttributes>) -> Self {
+        Error::UserInsertError(value)
     }
 }
--- data-migrations/directus-migration/src/main.rs
+++ data-migrations/directus-migration/src/main.rs
@@ -1,8 +1,10 @@
 mod codegen;
 mod error;
 use codegen::DirectusUser;
-use diesel_async::{AsyncConnection, AsyncPgConnection};
-use web_common_traits::database::Loadable;
+use core_structures::{User, UserEmail};
+use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
+use web_common_traits::database::{Insertable, InsertableBuilder, InsertableVariant, Loadable};
+use web_common_traits::prelude::Builder;
 
 const DATABASE_NAME: &str = "directus";
 const DATABASE_PASSWORD: &str = "directus_dbgi";
@@ -18,5 +20,12 @@ async fn main() -> Result<(), error::Error> {
     let mut conn = AsyncPgConnection::establish(DATABASE_URL).await?;
     let users = DirectusUser::load_all(&mut conn).await?;
     println!("We have loaded {} users", users.len());
+    for user in users {
+        let new_user = User::new()
+            .first_name(user.first_name.ok_or(error::Error::MissingFirstName(user.id))?)?
+            .last_name(user.last_name.ok_or(error::Error::MissingLastName(user.id))?)?
+            .build()?
+            .insert(&mut conn).await?;
+    }
     Ok(())
 }
