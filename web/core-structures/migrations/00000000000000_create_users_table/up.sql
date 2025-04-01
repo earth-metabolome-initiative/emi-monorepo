34c20d8b4a27e001edf2a959a13b78a3b53e080e
edouardbruelhart <edouard.bruelhart@unifr.ch>
2025-04-01 15:50:09 +0200

    changed from naive naive timestamps to timestamp with timezone

--- web/core-structures/migrations/00000000000000_create_users_table/up.sql
+++ web/core-structures/migrations/00000000000000_create_users_table/up.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Since users may have multiple organizations, we need a join table to represent this relationship
