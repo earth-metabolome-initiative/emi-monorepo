89710185799dbdabc4b57246e931a58e26ec172a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-04-08 09:09:09 +0200

    Updated library according to the updates in the graph and algebra crates

--- data-structures/mass-spectrometry/src/structs.rs
+++ data-structures/mass-spectrometry/src/structs.rs
@@ -2,8 +2,8 @@
 
 pub mod generic_spectrum;
 pub mod iterators;
-pub mod hungarian_cosine;
+pub mod exact_cosine;
 
 pub use generic_spectrum::GenericSpectrum;
 pub use iterators::GreedySharedPeaks;
-pub use hungarian_cosine::HungarianCosine;
+pub use exact_cosine::ExactCosine;
