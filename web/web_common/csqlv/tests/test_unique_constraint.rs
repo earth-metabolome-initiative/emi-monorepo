4841ca0d80b87208f2aee1196d60454987c2e96d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-04-10 15:13:36 +0200

    Extended testing in csqlv

--- web/web_common/csqlv/tests/test_unique_constraint.rs
+++ web/web_common/csqlv/tests/test_unique_constraint.rs
@@ -9,13 +9,17 @@ fn test_unique_constraint() -> Result<(), CSVSchemaError> {
     let band_table = schema.table_from_name("bands")?;
     let band_name = band_table.column_from_name("band")?;
     assert!(band_name.is_unique());
+    assert!(!band_name.is_nullable());
     let founded_by = band_table.column_from_name("founded_by")?;
     assert!(!founded_by.is_unique());
+    assert!(!founded_by.is_nullable());
     let foundation_year = band_table.column_from_name("foundation_year")?;
     assert!(foundation_year.is_unique());
+    assert!(!foundation_year.is_nullable());
 
     let instruments_table = schema.table_from_name("instruments")?;
     let instrument_name = instruments_table.column_from_name("name")?;
     assert!(instrument_name.is_unique());
+    assert!(!instrument_name.is_nullable());
     Ok(())
 }
