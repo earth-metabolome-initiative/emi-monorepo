df98dd57d22875f8497005efc0c4aebab93f3294
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-04-10 17:06:16 +0200

    Removed generic operation struct (to be replaced with enum)

--- web/web_common/web_common_traits/src/operation.rs
+++ web/web_common/web_common_traits/src/operation.rs
@@ -14,27 +14,3 @@ pub trait Operation: Basic {
     /// Returns the identifier of the operation.
     fn id(&self) -> rosetta_uuid::Uuid;
 }
-
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-/// Generic operation.
-pub struct GenericOperation<O> {
-    /// The identifier of the operation.
-    id: rosetta_uuid::Uuid,
-    /// The inner operation.
-    operation: O,
-}
-
-impl<O> Operation for GenericOperation<O>
-where
-    O: Operation,
-    <O as Operation>::Outcome: Outcome<Operation = Self>,
-    <O as Operation>::Error: OperationError<Operation = Self>,
-{
-    type Outcome = <O as Operation>::Outcome;
-    type Error = <O as Operation>::Error;
-
-    fn id(&self) -> rosetta_uuid::Uuid {
-        self.id
-    }
-}
