2fad53c1c43647ee3df78a1a1b67cd7cc688a9c8
Luca <cappelletti.luca94@gmail.com>
2025-04-21 12:21:07 +0200

    Moved linting to cargo toml

--- data-retrieval/downloader/src/lib.rs
+++ data-retrieval/downloader/src/lib.rs
@@ -1,12 +1,4 @@
 #![doc = include_str!("../README.md")]
-#![deny(missing_docs)]
-#![deny(unsafe_code)]
-#![deny(unused_macro_rules)]
-#![deny(unconditional_recursion)]
-#![deny(unreachable_patterns)]
-#![deny(unused_import_braces)]
-#![warn(clippy::all)]
-#![warn(clippy::pedantic)]
 
 pub mod compression_extension;
 pub mod downloader;
