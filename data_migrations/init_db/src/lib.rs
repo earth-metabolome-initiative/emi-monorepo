771b214a8bd92a44122865d975c354c67e7d93c0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-26 09:18:18 +0200

    Fixed code smells

--- data_migrations/init_db/src/lib.rs
+++ data_migrations/init_db/src/lib.rs
@@ -9,14 +9,15 @@ mod migrations;
 pub use init::init_database;
 
 /// Returns whether a database with the given name exists.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `database_name`: The name of the database to check.
-/// * `connection`: A mutable reference to an asynchronous PostgreSQL connection.
-/// 
+/// * `connection`: A mutable reference to an asynchronous `PostgreSQL`
+///   connection.
+///
 /// # Errors
-/// 
+///
 /// * If the query fails, an error of type `errors::Error` is returned.
 pub async fn database_exists(
     database_name: &str,
@@ -31,12 +32,9 @@ pub async fn database_exists(
         exists: i32,
     }
 
-    Ok(sql_query(format!(
-        "SELECT 1 as exists FROM pg_database WHERE datname = '{}'",
-        database_name
-    ))
-    .get_result::<Exists>(connection)
-    .await?
-    .exists
+    Ok(sql_query(format!("SELECT 1 as exists FROM pg_database WHERE datname = '{database_name}'",))
+        .get_result::<Exists>(connection)
+        .await?
+        .exists
         == 1)
 }
