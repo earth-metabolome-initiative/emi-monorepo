f8b190a2eba22e93eb618495950d6400d2963b59
oolonek <pierre-marie.allard@unifr.ch>
2025-09-09 16:43:52 +0200

    continued directus_migration tests

--- data_migrations/directus_migration/src/lib.rs
+++ data_migrations/directus_migration/src/lib.rs
@@ -2,6 +2,9 @@ mod codegen;
 mod migrations;
 use diesel::{Connection, PgConnection};
 
+use crate::codegen::FieldDatum;
+use web_common_traits::database::BoundedRead;
+
 const DIRECTUS_DATABASE_NAME: &str = "directus";
 const DIRECTUS_DATABASE_PASSWORD: &str = "directus_dbgi";
 const DIRECTUS_DATABASE_USER: &str = "directus";
@@ -10,6 +13,11 @@ const DIRECTUS_HOSTNAME: &str = "134.21.20.118";
 const DIRECTUS_DATABASE_URL: &str = const_format::formatcp!(
     "postgres://{DIRECTUS_DATABASE_USER}:{DIRECTUS_DATABASE_PASSWORD}@{DIRECTUS_HOSTNAME}:{DIRECTUS_DATABASE_PORT}/{DIRECTUS_DATABASE_NAME}",
 );
+
+pub fn directus_connection() -> Result<PgConnection, anyhow::Error> {
+    let conn = PgConnection::establish(DIRECTUS_DATABASE_URL)?;
+    Ok(conn)
+}
 
 const PORTAL_DATABASE_NAME: &str = "development.db";
 const PORTAL_DATABASE_PASSWORD: &str = "password";
@@ -21,10 +29,13 @@ const PORTAL_DATABASE_URL: &str = const_format::formatcp!(
 
 /// Executes the data migration from the Directus database to the portal
 /// database.
-fn transact_migration(
+pub fn directus_migration(
     directus_conn: &mut PgConnection,
     portal_conn: &mut PgConnection,
 ) -> Result<(), anyhow::Error> {
+    for field_data_row in FieldDatum::bounded_read(0, 5, directus_conn)? {
+        println!("Field data row: {:?}", field_data_row);
+    }
     // insert_missing_users(directus_conn, portal_conn)?;
     // insert_missing_brands(directus_conn, portal_conn)?;
     // insert_missing_instrument_models(directus_conn, portal_conn)?;
@@ -32,12 +43,12 @@ fn transact_migration(
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    let mut directus_conn = PgConnection::establish(DIRECTUS_DATABASE_URL)?;
-    let mut portal_conn = PgConnection::establish(PORTAL_DATABASE_URL)?;
+// #[tokio::main]
+// async fn main() -> Result<(), anyhow::Error> {
+//     let mut directus_conn = PgConnection::establish(DIRECTUS_DATABASE_URL)?;
+//     let mut portal_conn = PgConnection::establish(PORTAL_DATABASE_URL)?;
 
-    portal_conn.transaction(|portal_conn| transact_migration(&mut directus_conn, portal_conn))?;
+//     portal_conn.transaction(|portal_conn| transact_migration(&mut directus_conn, portal_conn))?;
 
-    Ok(())
-}
+//     Ok(())
+// }
--- data_migrations/directus_migration/tests/test_directus_migration.rs
+++ data_migrations/directus_migration/tests/test_directus_migration.rs
@@ -2,12 +2,12 @@
 //! reference docker and populated with the `init_migration`.
 
 use core_structures::LoginProvider;
-use core_structures_vis::MermaidDB;
+use directus_migration::directus_connection;
+use directus_migration::directus_migration;
 use init_db::init_database;
 use init_migration::{init_dbgi_plan, init_migration, init_root_user};
 use reference_docker::reference_docker_with_connection;
 use web_common_traits::database::BoundedRead;
-use directus_migration::directus_migration;
 
 const DATABASE_NAME: &str = "test_directus_migration.db";
 const DATABASE_PORT: u16 = 12132;
@@ -16,19 +16,22 @@ const DATABASE_PORT: u16 = 12132;
 async fn test_directus_migration() {
     dotenvy::from_path("../../web/.env_develop").unwrap();
 
+    let mut directus_conn = directus_connection().expect("Failed to connect to the Directus database");
+
     // Get the output directory
-    let (docker, mut conn) = reference_docker_with_connection(DATABASE_NAME, DATABASE_PORT)
+    let (docker, mut portal_conn) = reference_docker_with_connection(DATABASE_NAME, DATABASE_PORT)
         .await
         .expect("Failed to connect to the database");
 
     // We initialize the database into the docker container
-    if let Err(err) = init_database(DATABASE_NAME, true, &mut conn).await {
+    if let Err(err) = init_database(DATABASE_NAME, true, &mut portal_conn).await {
         docker.stop().await.expect("Failed to stop the docker container");
         panic!("Failed to initialize the database: {err}");
     }
 
     // We try to populate the DB with the init initialization
-    if let Err(err) = directus_migration(&mut conn) {
+    if let Err(err) = directus_migration(&mut directus_conn, &mut portal_conn) {
+     
         docker.stop().await.expect("Failed to stop the docker container");
         panic!("Failed to execute the Directus migration: {err}");
     }
