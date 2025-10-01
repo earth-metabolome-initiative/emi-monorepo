4424d19b553c7fd74e25f39e249dcfe2599b3006
Luca <cappelletti.luca94@gmail.com>
2025-10-01 21:09:56 +0200

    Fixed issue in `RandomizedDAG` and patched criterion docs error

--- data_structures/graph/benches/wu_palmer.rs
+++ data_structures/graph/benches/wu_palmer.rs
@@ -4,7 +4,7 @@ use std::hint::black_box;
 
 use algebra::impls::{CSR2D, SquareCSR2D};
 use criterion::{Criterion, criterion_group, criterion_main};
-use functional_properties::similarity::{self, ScalarSimilarity};
+use functional_properties::similarity::ScalarSimilarity;
 use graph::{
     prelude::{GenericGraph, RandomizedDAG},
     traits::{MonopartiteGraph, WuPalmer, randomized_graphs::XorShift64},
@@ -15,11 +15,11 @@ fn bench_wu_palmer(c: &mut Criterion) {
     c.bench_function("wu_palmer_100", |b| {
         const NUMBER_OF_DAGS: usize = 10;
         let mut dags = Vec::with_capacity(NUMBER_OF_DAGS);
-        let mut xorshift = XorShift64::new(24537839457);
+        let mut xorshift = XorShift64::from(24537839457);
         for _ in 0..NUMBER_OF_DAGS {
-            let seed = xorshift.next();
-            let dag: GenericGraph<usize, SquareCSR2D<CSR2D<usize, usize, usize>>> =
-                RandomizedDAG::randomized_dag(seed, 100);
+            let seed = xorshift.next().unwrap();
+            let dag: GenericGraph<u64, SquareCSR2D<CSR2D<u64, u64, u64>>> =
+                RandomizedDAG::randomized_dag(seed, 15);
             dags.push(dag);
         }
         b.iter(|| {
--- data_structures/graph/src/traits/algorithms/randomized_graphs/randomized_dag.rs
+++ data_structures/graph/src/traits/algorithms/randomized_graphs/randomized_dag.rs
@@ -3,10 +3,19 @@
 
 use std::collections::HashSet;
 
-use algebra::prelude::{SparseMatrixMut, SquareMatrix};
+use algebra::prelude::SparseMatrixMut;
 use common_traits::prelude::Builder;
-use crate::prelude::generic_monoplex_monopartite_graph_builder::MonoplexMonopartiteGraphBuilderError;
-use crate::{prelude::GenericMonoplexMonopartiteGraphBuilder, traits::{edge, GrowableEdges, MonopartiteEdges, MonopartiteGraphBuilder, MonoplexGraph, MonoplexGraphBuilder, MonoplexMonopartiteGraph}};
+
+use crate::{
+    prelude::{
+        GenericMonoplexMonopartiteGraphBuilder,
+        generic_monoplex_monopartite_graph_builder::MonoplexMonopartiteGraphBuilderError,
+    },
+    traits::{
+        GrowableEdges, MonopartiteGraphBuilder, MonoplexGraph, MonoplexGraphBuilder,
+        MonoplexMonopartiteGraph,
+    },
+};
 
 /// Trait providing randomized dag method
 pub trait RandomizedDAG: MonoplexGraph {
@@ -20,54 +29,60 @@ pub trait RandomizedDAG: MonoplexGraph {
 
 impl<G> RandomizedDAG for G
 where
-    G: MonoplexMonopartiteGraph<NodeId = u64, Nodes = u64> + TryFrom<(G::Nodes,G::MonoplexMonopartiteEdges), Error = MonoplexMonopartiteGraphBuilderError>,
-    G::MonoplexMonopartiteEdges: GrowableEdges <EdgeId = u64, Edge = (u64,u64),> + MonopartiteEdges<NodeId = u64>,
-    <G::MonoplexMonopartiteEdges as GrowableEdges>::GrowableMatrix:SparseMatrixMut<MinimalShape = u64>
+    G: MonoplexMonopartiteGraph<Nodes = u64>
+        + TryFrom<
+            (G::Nodes, G::MonoplexMonopartiteEdges),
+            Error = MonoplexMonopartiteGraphBuilderError,
+        >,
+    G::MonoplexMonopartiteEdges: GrowableEdges<EdgeId = u64, Edge = (u64, u64)>,
+    <G::MonoplexMonopartiteEdges as GrowableEdges>::GrowableMatrix:
+        SparseMatrixMut<MinimalShape = u64>,
 {
     fn randomized_dag(seed: u64, nodes: u64) -> Self {
-        let mut xorshift  = XorShift64::new(seed);
-        let number_of_edges = xorshift.next() % (nodes * (nodes-1)/2);
-        let mut edge_tuples = HashSet::with_capacity(number_of_edges as usize);
-        while edge_tuples.len() < number_of_edges as usize {
-            let seed1 = xorshift.next();
-            let seed2 = xorshift.next();
+        let mut xorshift = XorShift64::from(seed);
+        let number_of_edges = xorshift.next().unwrap() % (nodes * (nodes - 1) / 2);
+        let mut edge_tuples = HashSet::with_capacity(usize::try_from(number_of_edges).unwrap());
+        while edge_tuples.len() < usize::try_from(number_of_edges).unwrap() {
+            let seed1 = xorshift.next().unwrap();
+            let seed2 = xorshift.next().unwrap();
             let mut src = seed1 % nodes;
             let mut dst = seed2 % nodes;
+            if src == dst {
+                continue;
+            }
             if src > dst {
                 core::mem::swap(&mut src, &mut dst);
             }
-            edge_tuples.insert((src,dst));
+            edge_tuples.insert((src, dst));
         }
-        let mut sorted_edge_tuples:Vec<(u64,u64)> = edge_tuples.into_iter().collect();
+        let mut sorted_edge_tuples: Vec<(u64, u64)> = edge_tuples.into_iter().collect();
         sorted_edge_tuples.sort_unstable();
         let mut edges = G::MonoplexMonopartiteEdges::with_shaped_capacity(nodes, number_of_edges);
         for (src, dst) in sorted_edge_tuples {
-            edges.add((src,dst)).unwrap();
+            edges.add((src, dst)).unwrap();
         }
         GenericMonoplexMonopartiteGraphBuilder::default().nodes(nodes).edges(edges).build().unwrap()
-
     }
 }
 
-/// Struct for storing the XorShift state
-pub struct XorShift64 {
-    state: u64,
+/// Struct for storing the `XorShift64` state
+pub struct XorShift64(u64);
+
+impl From<u64> for XorShift64 {
+    fn from(state: u64) -> Self {
+        Self(state)
+    }
 }
 
-impl XorShift64 {
-    /// Creates a new XorShift64 generator with a non-zero seed
-    pub fn new(seed: u64) -> Self {
-        assert!(seed != 0, "Seed must be non-zero");
-        Self { state: seed }
-    }
+impl Iterator for XorShift64 {
+    type Item = u64;
 
-    /// Generates the next random u64
-    pub fn next(&mut self) -> u64 {
-        let mut x = self.state;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut x = self.0;
         x ^= x << 13;
         x ^= x >> 7;
         x ^= x << 17;
-        self.state = x;
-        x
+        self.0 = x;
+        Some(x)
     }
 }
