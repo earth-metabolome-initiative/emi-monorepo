2033fbdc5c89438f4481ec7b56c57f242f635280
Luca <cappelletti.luca94@gmail.com>
2025-10-07 16:21:04 +0200

    Finished removing all constraints from codegen

--- web/web_common/webcodegen/src/custom_schema_constraints.rs
+++
@@ -1,7 +0,0 @@
-mod constraint_errors;
-mod custom_schema_constraint;
-mod word_deprecation_constraint;
-
-pub use constraint_errors::ConstraintError;
-pub use custom_schema_constraint::{CustomColumnConstraint, CustomTableConstraint};
-pub use word_deprecation_constraint::WordDeprecationConstraint;
