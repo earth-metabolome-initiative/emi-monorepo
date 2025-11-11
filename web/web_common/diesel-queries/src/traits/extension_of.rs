fabae5a1fe6670ed6777d133c4e62512154ec803
Luca <cappelletti.luca94@gmail.com>
2025-11-11 15:49:41 +0100

    Tried to simplify ExtensionOf trait

--- web/web_common/diesel-queries/src/traits/extension_of.rs
+++ web/web_common/diesel-queries/src/traits/extension_of.rs
@@ -7,11 +7,11 @@ use diesel::Identifiable;
 /// Trait representing an object that is an extension of another object.
 pub trait ExtensionOf<Extended>
 where
-    for<'ext> &'ext Extended: Identifiable,
-    for<'ext> &'ext Self: Identifiable<Id = <&'ext Extended as Identifiable>::Id>,
+    for<'ext> &'ext Self: Identifiable,
 {
     /// The type of the extended object.
     type ExtendedType<'data>: Borrow<Extended>
+        + Identifiable<Id = <&'data Self as Identifiable>::Id>
     where
         Self: 'data;
 }
@@ -20,8 +20,7 @@ where
 /// (the extended object) from a database connection.
 pub trait Ancestor<Extended, C>: ExtensionOf<Extended>
 where
-    for<'ext> &'ext Extended: Identifiable,
-    for<'ext> &'ext Self: Identifiable<Id = <&'ext Extended as Identifiable>::Id>,
+    for<'ext> &'ext Self: Identifiable,
 {
     /// Returns the extended object.
     fn ancestor(&self, connection: &mut C)
