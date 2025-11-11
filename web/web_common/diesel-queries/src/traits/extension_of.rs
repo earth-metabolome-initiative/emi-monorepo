d9711987bfadb88b16c7662562c48150cb33048f
Luca <cappelletti.luca94@gmail.com>
2025-11-11 16:34:48 +0100

    Refactored diesel queries

--- web/web_common/diesel-queries/src/traits/extension_of.rs
+++ web/web_common/diesel-queries/src/traits/extension_of.rs
@@ -5,11 +5,17 @@ use std::borrow::Borrow;
 use diesel::Identifiable;
 
 /// Trait representing an object that is an extension of another object.
-pub trait ExtensionOf<Extended>
+pub trait ExtensionOfHelper<Extended> {}
+
+impl<Extended, T> ExtensionOfHelper<Extended> for T
 where
     for<'a> &'a Extended: Identifiable,
-    for<'a> &'a Self: Identifiable<Id = <&'a Extended as Identifiable>::Id>,
+    for<'a> &'a T: Identifiable<Id = <&'a Extended as Identifiable>::Id>,
 {
+}
+
+/// Trait representing an object that is an extension of another object.
+pub trait ExtensionOf<Extended>: ExtensionOfHelper<Extended> {
     /// The type of the extended object.
     type ExtendedType<'data>: Borrow<Extended>
     where
@@ -18,11 +24,7 @@ where
 
 /// Trait representing an object that can retrieve its ancestor
 /// (the extended object) from a database connection.
-pub trait Ancestor<Extended, C>: ExtensionOf<Extended>
-where
-    for<'a> &'a Extended: Identifiable,
-    for<'a> &'a Self: Identifiable<Id = <&'a Extended as Identifiable>::Id>,
-{
+pub trait Ancestor<Extended, C>: ExtensionOf<Extended> {
     /// Returns the extended object.
     fn ancestor(&self, connection: &mut C)
     -> Result<Self::ExtendedType<'_>, diesel::result::Error>;
