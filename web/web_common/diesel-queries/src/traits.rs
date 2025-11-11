46ab8262cef56ae1b30cef83e1067e6bc83bd878
Luca <cappelletti.luca94@gmail.com>
2025-11-11 16:00:57 +0100

    Tried to simplify Identifiable constraint

--- web/web_common/diesel-queries/src/traits.rs
+++ web/web_common/diesel-queries/src/traits.rs
@@ -4,3 +4,5 @@ pub mod read;
 pub use read::Read;
 pub mod extension_of;
 pub use extension_of::{ExtensionOf, Ancestor};
+pub mod identifiable_ref;
+pub use identifiable_ref::IdentifiableRef;
--- web/web_common/diesel-queries/src/traits/extension_of.rs
+++ web/web_common/diesel-queries/src/traits/extension_of.rs
@@ -4,24 +4,20 @@ use std::borrow::Borrow;
 
 use diesel::Identifiable;
 
+use crate::traits::IdentifiableRef;
+
 /// Trait representing an object that is an extension of another object.
-pub trait ExtensionOf<Extended>
-where
-    for<'ext> &'ext Self: Identifiable,
-{
+pub trait ExtensionOf<Extended>: IdentifiableRef {
     /// The type of the extended object.
     type ExtendedType<'data>: Borrow<Extended>
-        + Identifiable<Id = <&'data Self as Identifiable>::Id>
+        + Identifiable<Id = <Self as IdentifiableRef>::Id<'data>>
     where
         Self: 'data;
 }
 
 /// Trait representing an object that can retrieve its ancestor
 /// (the extended object) from a database connection.
-pub trait Ancestor<Extended, C>: ExtensionOf<Extended>
-where
-    for<'ext> &'ext Self: Identifiable,
-{
+pub trait Ancestor<Extended, C>: ExtensionOf<Extended> {
     /// Returns the extended object.
     fn ancestor(&self, connection: &mut C)
     -> Result<Self::ExtendedType<'_>, diesel::result::Error>;
