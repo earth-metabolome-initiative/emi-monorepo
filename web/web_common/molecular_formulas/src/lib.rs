61fa18c1b843fc25796a55c3ccb9c3648c66eb57
Luca Cappelletti <cappelletti.luca94@gmail.com>
2025-12-17 16:20:41 +0100

    Molecule from iter (#230)
    * Now molecular formula can be built from char iter
    
    * Resolved code smells
    
    * Reformatted code
    
    * Added support for chained charges
    
    * Added support for manipulating token iter
    
    * Exposed chars iter
    
    * Added support for tracking lowercaseness in atoms
    
    * Added the smiles parsing exceptions to elements
    
    * Added more smiles exceptions
    
    * Formatted code
    
    * Fixed clippy code smells
    
    * Exposed publicly atom struc

--- web/web_common/molecular_formulas/src/lib.rs
+++ web/web_common/molecular_formulas/src/lib.rs
@@ -6,8 +6,8 @@
 mod ion;
 pub mod molecular_formula;
 pub mod parser;
-mod token;
+pub mod token;
 pub use ion::Ion;
 pub use molecular_formula::MolecularFormula;
-pub use token::{Token, greek_letters::GreekLetter};
+pub use token::{Token, Atom, greek_letters::GreekLetter};
 pub mod errors;
