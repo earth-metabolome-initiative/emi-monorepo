4480b09f696674e3a9330f40c0b32af28ac0fb9d
Luca <cappelletti.luca94@gmail.com>
2025-12-28 13:38:49 +0100

    Removed prelude due to confusion it was inducing

--- web/web_common/pg_diesel/src/lib.rs
+++ web/web_common/pg_diesel/src/lib.rs
@@ -7,11 +7,3 @@ pub mod schema;
 pub mod traits;
 pub use database::PgDatabase;
 pub mod model_metadata;
-
-/// Prelude module re-exporting commonly used items.
-pub mod prelude {
-	pub use crate::database::*;
-	pub use crate::traits::*;
-	pub use crate::models::*;
-	pub use sql_traits::prelude::*;
-}
