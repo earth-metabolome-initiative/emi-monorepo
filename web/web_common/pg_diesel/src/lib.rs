6ad9fcacd96b3109015e26da9f65643d0c78cc7b
Luca <cappelletti.luca94@gmail.com>
2026-01-04 17:43:49 +0100

    Moved pg-diesel out

--- web/web_common/pg_diesel/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![doc = include_str!("../README.md")]
-
-pub mod database;
-pub mod impls;
-pub mod models;
-pub mod schema;
-pub mod traits;
-pub use database::PgDieselDatabase;
-pub mod model_metadata;
--- web/web_common/sql_relations/src/impls.rs
+++ web/web_common/sql_relations/src/impls.rs
@@ -1,5 +1,4 @@
 //! Submodule providing implementations of the traits defined in the
-//! [`traits`](crate::traits) submodule for the structs defined in the
-//! [`pg_diesel`](web_common::pg_diesel) crate.
+//! [`traits`](crate::traits) submodule for the structs defined in `sqlparser`.
 
 mod sqlparser;
